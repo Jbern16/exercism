0e879df7315c4e24d3ca7e3bf957bd3d98709a1e
Jbern16 <jbern16@gmail.com>
2016-05-06 14:44:40 -0400

    add javascript

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,14 @@
+module.exports = Hamming;
+
+var Hamming = function(){};
+
+Hamming.prototype.compute = function(pair1, pair2){
+  var splitPair1 = pair1.split(",");
+  var splitPair2 = pair2.split(",");
+  var same = splitPair1.filter(function(, i (splitPair1[i] === splitPair2[i] ) 
+
+    }
+  });
+
+
+}
