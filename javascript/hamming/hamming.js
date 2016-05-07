3023ad6f29ab9a15ff11bb7581855e4cf8fec6a7
Jbern16 <jbern16@gmail.com>
2016-05-07 16:31:34 -0400

    add js

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,14 +1,18 @@
-module.exports = Hamming;
-
 var Hamming = function(){};
 
 Hamming.prototype.compute = function(pair1, pair2){
-  var splitPair1 = pair1.split(",");
-  var splitPair2 = pair2.split(",");
-  var same = splitPair1.filter(function(, i (splitPair1[i] === splitPair2[i] ) 
+  var splitPair1 = pair1.split("");
+  var splitPair2 = pair2.split("");
+  var mutations = [];
 
+  if (splitPair1.length !== splitPair2.length)
+    throw new Error ('DNA strands must be of equal length.')
+  else
+    for (var i = 0; i < splitPair1.length; i++) {
+      if (splitPair1[i] !== splitPair2[i])
+        mutations.push(splitPair1[i]);
     }
-  });
+    return mutations.length;
+};
 
-
-}
+module.exports = Hamming;
