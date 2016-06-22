6296f9b8a0d5cd67cd8e26bd7297fb4c73d69a73
Jbern16 <jbern16@gmail.com>
2016-06-22 11:39:08 -0400

    rna-transscription;

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,27 @@
+var DnaTranscriber = function(){}
+
+DnaTranscriber.prototype.toRna = function(dna){
+  var split = dna.split("");
+  var transcribed  = split.map(function(letter){
+    if(letter == 'G')
+      return 'C'
+    else if(letter == 'C')
+      return 'G'
+    else if(letter == 'T')
+      return 'A'
+    else if(letter == 'A')
+      return 'U'
+  })
+
+  return transcribed.join("")
+}
+
+module.exports = DnaTranscriber
+
+
+
+
+// * `G` -> `C`
+// * `C` -> `G`
+// * `T` -> `A`
+// * `A` -> `U`
